54be522b1dc1a74e04cf184c1a089ccbdc0076b2
Johnson Taylor <johnson.taylor@griffithuni.edu.au>
2021-05-26 03:11:13 +1000

    Added location, locationViewModel, LazyTextField

---
+++ EateriesCD/Location.swift
@@ -0,0 +1,14 @@
+//
+//  Location.swift
+//  EateriesCD
+//
+//  Created by Johnson Taylor on 26/5/21.
+//
+
+import Foundation
+
+struct Location {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
